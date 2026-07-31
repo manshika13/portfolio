3082141f0a3b662bfe0ea0b2ee36114205001e4c
Manshika <102040383+manshika13@users.noreply.github.com>
2026-07-31 23:17:45 +0530

    Modify base path and enable minification in config
    Update base path conditionally based on build mode and enable minification.

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/portfolio/',
+    base: isBuild ? '/portfolio/':'/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
@@ -18,6 +18,7 @@ export default defineConfig(({mode}) => {
     },
     build:{
       outDir: 'dist',
+      minify: true,
     },
     server: {
       // HMR is disabled in AI Studio via DISABLE_HMR env var.
